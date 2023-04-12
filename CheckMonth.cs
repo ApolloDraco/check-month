dd930bdbfe2532d802c1c358f3e7a63b5fa98599
Apollo <88677238+ApolloDraco@users.noreply.github.com>
2023-04-12 02:34:48 -0400

    Update CheckMonth.cs

--- CheckMonth.cs
+++ CheckMonth.cs
@@ -1,36 +1,35 @@
-using static System.Console;
+/*
+* This program prompts the user to enter their birth month and day.
+* It then checks if the inputted month and day form a valid date.
+* If the inputted date is valid, the program outputs a message indicating that it is a valid birthday.
+* If the inputted date is invalid, the program outputs a message indicating that it is an invalid date.
+*/
+
 using System;
 
 class CheckMonth
 {
-static void Main()
-{
-int month, day;
-//Prompts a user to enter a birth month and day.
-Console.Write("Please enter your month of birth: ");
-month = Convert.ToInt32(Console.ReadLine());
-Console.Write("Please enter your day of birth: ");
-day = Convert.ToInt32(Console.ReadLine());
-//Display an error message if the month is invalid (not 1 through 12)
-//or the day is invalid for the month
-//If the month and day are valid, display them with a message.
-if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
-if(day <= 31 && day >= 1)
-Console.WriteLine(month+"/"+day+" is a valid birthday.");
-else
-Console.WriteLine("Invalid date");
-else if(month == 4 || month == 6 || month == 9 || month == 11)
-if(day <= 30 && day >= 1)
-Console.WriteLine(month+"/"+day+" is a valid birthday.");
-else
-Console.WriteLine("Invalid date");
-else if(month == 2)
-if(day <= 29 && day >= 1)
-Console.WriteLine(month+"/"+day+" is a valid birthday.");
-else
-Console.WriteLine("Invalid date");
-else
-Console.WriteLine("Invalid date");
-
-}
+    static void Main()
+    {
+        int month, day;
+        
+        // Prompts the user to enter their birth month and day
+        Console.Write("Please enter your month of birth (1-12): ");
+        
+        if (!int.TryParse(Console.ReadLine(), out month) || month < 1 || month > 12)
+        {
+            Console.WriteLine("Invalid month");
+            return;
+        }
+        
+        Console.Write("Please enter your day of birth: ");
+        
+        if (!int.TryParse(Console.ReadLine(), out day) || day < 1 || day > DateTime.DaysInMonth(DateTime.Now.Year, month))
+        {
+            Console.WriteLine("Invalid day");
+            return;
+        }
+        
+        Console.WriteLine($"{month}/{day} is a valid birthday.");
+    }
 }
